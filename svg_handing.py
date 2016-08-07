bc9abcf0b844933794c683b65817df99abebf29f
Janis Lesinskis <janis.lesinskis@biarri.com>
2016-08-07 23:48:23 +1000

    Refactor of parameters passed in

--- svg_handing.py
+++ svg_handing.py
@@ -4,21 +4,22 @@ from seirpinski import Triangle, get_subtriangle_vertices
 from utils import flattener
 
 
-def svg_sierpinski(dwg, depth):
+def svg_sierpinski(dwg, depth, initial_triangle):
     """
+    Create the lines in a sierpinski triangle
     :dwg: the svg Drawing object
     :depth: the depth of the Sirepinski triangle being created
+    :initial_triangle: the outermost triangle
     """
-    right_angled_triangle = Triangle((0, 0), (0, 100), (100, 0))
-    subtriangles_test = get_subtriangle_vertices(right_angled_triangle, depth)
-    flattened = flattener(subtriangles_test)
+    subtriangles = get_subtriangle_vertices(initial_triangle, depth)
+    flattened = flattener(subtriangles)
     for triangle in flattened:
-        print(triangle)
         dwg.add(dwg.line(triangle.v1, triangle.v2, stroke='black'))
         dwg.add(dwg.line(triangle.v2, triangle.v3, stroke='black'))
         dwg.add(dwg.line(triangle.v3, triangle.v1, stroke='black'))
 
 if __name__ == "__main__":
-    dwg = svgwrite.Drawing(filename='test.svg', size=("800px", "600px"), profile='tiny')
-    svg_sierpinski(dwg, 3)
+    right_angled_triangle = Triangle((0, 0), (0, 600), (600, 0))
+    dwg = svgwrite.Drawing(filename='test.svg', size=("600px", "600px"), profile='tiny')
+    svg_sierpinski(dwg, depth=5, initial_triangle=right_angled_triangle)
     dwg.save()
